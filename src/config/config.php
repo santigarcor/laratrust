0707b33569256c297ba386e902a2c1177ef3b91a
Santiago García <santigarcor@gmail.com>
2016-05-25 23:07:42 -0500

    Changed config.php and composer.lock

--- src/config/config.php
+++ src/config/config.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
- * This file is part of Entrust,
+ * This file is part of Laratrust,
  * a role & permission management solution for Laravel.
  *
  * @license MIT
- * @package Zizaco\Entrust
+ * @package Santigarcor\Laratrust
  */
 
 return [
 
     /*
     |--------------------------------------------------------------------------
-    | Entrust Role Model
+    | Laratrust Role Model
     |--------------------------------------------------------------------------
     |
-    | This is the Role model used by Entrust to create correct relations.  Update
+    | This is the Role model used by Laratrust to create correct relations.  Update
     | the role if it is in a different namespace.
     |
     */
@@ -23,20 +23,20 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Entrust Roles Table
+    | Laratrust Roles Table
     |--------------------------------------------------------------------------
     |
-    | This is the roles table used by Entrust to save roles to the database.
+    | This is the roles table used by Laratrust to save roles to the database.
     |
     */
     'roles_table' => 'roles',
 
     /*
     |--------------------------------------------------------------------------
-    | Entrust Permission Model
+    | Laratrust Permission Model
     |--------------------------------------------------------------------------
     |
-    | This is the Permission model used by Entrust to create correct relations.
+    | This is the Permission model used by Laratrust to create correct relations.
     | Update the permission if it is in a different namespace.
     |
     */
@@ -44,10 +44,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Entrust Permissions Table
+    | Laratrust Permissions Table
     |--------------------------------------------------------------------------
     |
-    | This is the permissions table used by Entrust to save permissions to the
+    | This is the permissions table used by Laratrust to save permissions to the
     | database.
     |
     */
@@ -55,10 +55,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Entrust permission_role Table
+    | Laratrust permission_role Table
     |--------------------------------------------------------------------------
     |
-    | This is the permission_role table used by Entrust to save relationship
+    | This is the permission_role table used by Laratrust to save relationship
     | between permissions and roles to the database.
     |
     */
@@ -66,10 +66,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Entrust role_user Table
+    | Laratrust role_user Table
     |--------------------------------------------------------------------------
     |
-    | This is the role_user table used by Entrust to save assigned roles to the
+    | This is the role_user table used by Laratrust to save assigned roles to the
     | database.
     |
     */
@@ -77,21 +77,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | User Foreign key on Entrust's role_user Table (Pivot)
+    | User Foreign key on Laratrust's role_user Table (Pivot)
     |--------------------------------------------------------------------------
     */
     'user_foreign_key' => 'user_id',
 
     /*
     |--------------------------------------------------------------------------
-    | Role Foreign key on Entrust's role_user and permission_role Tables (Pivot)
+    | Role Foreign key on Laratrust's role_user and permission_role Tables (Pivot)
     |--------------------------------------------------------------------------
     */
     'role_foreign_key' => 'role_id',
 
     /*
     |--------------------------------------------------------------------------
-    | Permission Foreign key on Entrust's permission_role Table (Pivot)
+    | Permission Foreign key on Laratrust's permission_role Table (Pivot)
     |--------------------------------------------------------------------------
     */
     'permission_foreign_key' => 'permission_id',
