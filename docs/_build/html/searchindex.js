7175e7fdb3d8cd6de5a41fa7ff53be8fb84de9a7
Santiago García <santigarcor@gmail.com>
2016-09-14 18:29:05 -0500

    removed build folder

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Laratrust Docs&#8217;s documentation!"],titleterms:{doc:0,document:0,indice:0,laratrust:0,tabl:0,welcom:0}})
