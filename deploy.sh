79cd79dbbee3431218e45611774793958cf3d925
Santiago García <santigarcor@gmail.com>
2018-08-11 11:36:15 -0500

    Add git uignore

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:santigarcor/laratrust.git master:gh-pages
+git push -f git@github.com:santigarcor/laratrust.git master:gh-pages
 
 cd -
