dadd51650ec69f583ecea36384efeaceea9a3877
Santiago García <santigarcor@gmail.com>
2018-08-12 22:42:21 -0500

    Improve docs-link component for docs versions

--- docs/.vuepress/components/DocsLink.vue
+++ docs/.vuepress/components/DocsLink.vue
@@ -9,11 +9,21 @@
     props: ['to'],
     computed: {
       finalTo() {
-        let tempTo = this.to.startsWith('/')
+        const currentPageVersion = this.getCurrentPageVersion();
+        const activeVersion = this.$site.themeConfig.activeVersion.link;
+        const tempTo = this.to.startsWith('/')
           ? this.to.slice(1)
           : this.to;
+        const versionLink = activeVersion == currentPageVersion
+          ? this.$site.themeConfig.activeVersion.link
+          : `/docs/${currentPageVersion}/`;
 
-        return `${this.$site.themeConfig.activeVersion.link}${tempTo}`;
+        return `${versionLink}${tempTo}`;
+      }
+    },
+    methods: {
+      getCurrentPageVersion() {
+        return this.$page.path.match(/([0-9]*[.])?[0-9]+/)[0];
       }
     },
   }
