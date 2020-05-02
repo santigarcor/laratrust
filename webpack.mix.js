6615a7f31a04f9474241886f8a503140d643e14d
Santiago Garcia <santigarcor@gmail.com>
2020-05-02 17:02:39 -0500

    Add tailwind processing for the panel

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.setPublicPath('public')
+  .postCss('resources/css/styles.css', 'public/laratrust.css', [
+    require('tailwindcss'),
+  ])
+  .copy('docs/.vuepress/public/logo.png', 'public/img/logo.png');
