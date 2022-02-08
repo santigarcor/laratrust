1471c9303294eec81775576e982515145024e7f7
Santiago Garcia <santigarcor@gmail.com>
2022-02-08 15:50:20 -0500

    Fix mix imports

--- resources/views/panel/layout.blade.php
+++ resources/views/panel/layout.blade.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="shortcut icon" href="{{ asset('/vendor/laratrust/img/logo.png') }}">
+  <link rel="shortcut icon" href="/vendor/laratrust/img/logo.png">
   <title>Laratrust - @yield('title')</title>
-  <link href="{{ asset(mix('laratrust.css', 'vendor/laratrust')) }}" rel="stylesheet">
+  <link href="{{ mix('laratrust.css', 'vendor/laratrust') }}" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body>
