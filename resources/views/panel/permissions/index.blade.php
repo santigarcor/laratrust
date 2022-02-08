2263a22c1255d3599be139b70ba387c09b3c6b10
Santiago Garcia <santigarcor@gmail.com>
2022-02-08 13:32:33 -0500

    Add configuration option to hide permissions creation from the panel

--- resources/views/panel/permissions/index.blade.php
+++ resources/views/panel/permissions/index.blade.php
@@ -4,12 +4,14 @@
 
 @section('content')
   <div class="flex flex-col">
+    @if (config('laratrust.panel.create_permissions'))
     <a
       href="{{route('laratrust.permissions.create')}}"
       class="self-end bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
     >
       + New Permission
     </a>
+    @endif
     <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
       <div class="mt-4 align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
         <table class="min-w-full">
