22525b2ca18c339268a3f745efa9473ed46e72ab
Santiago Garcia <santigarcor@gmail.com>
2022-02-11 11:51:51 -0500

    Match command declaration with symfony command class

--- src/Console/AddLaratrustUserTraitUseCommand.php
+++ src/Console/AddLaratrustUserTraitUseCommand.php
@@ -66,7 +66,7 @@ class AddLaratrustUserTraitUseCommand extends Command
      *
      * @return string
      */
-    public function getDescription()
+    public function getDescription(): string
     {
         return "Add LaratrustUserTrait to {$this->getUserModels()->implode(', ')} class";
     }
