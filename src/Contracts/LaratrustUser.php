a35abe7a26cef1c2ffef5f4d623d52576b7decfa
Incoming <thaipop@gmail.com>
2025-01-16 22:33:50 +0700

    Fix php 8.4 deprecated types (#688)
    * Fix php 8.4 deprecated types
    
    * Change $team to mixed type
    
    * Add 8.4 in GitHub tests
    
    ---------
    
    Co-authored-by: Mickael <mickael.desmousseaux@rapid4cloud.com>
    
    Fixes #685

--- src/Contracts/LaratrustUser.php
+++ src/Contracts/LaratrustUser.php
@@ -150,5 +150,5 @@ interface LaratrustUser
      *
      * @return Collection<\Laratrust\Contracts\Permission>
      */
-    public function allPermissions(array $columns = null, $team = false): Collection;
+    public function allPermissions(?array $columns = null, mixed $team = false): Collection;
 }
